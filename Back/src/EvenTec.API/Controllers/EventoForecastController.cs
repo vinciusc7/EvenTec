2fba371e0d1d840746cceaae61ff14ca1fbcebba
Inotec19 <inotec30@mail.com>
2022-07-24 13:44:05 -0300

    Modulo 3: EF 5

--- Back/src/EvenTec.API/Controllers/EventoForecastController.cs
+++ Back/src/EvenTec.API/Controllers/EventoForecastController.cs
@@ -5,63 +5,43 @@ using System.Threading.Tasks;
 using EvenTec.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using EvenTec.API.Data;
 namespace EvenTec.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-            
+            _context = context;
         }
-
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento () {
-            EventoId = 1,
-            Local = "Jardim Alvorada",
-            Lote = "1º Lote",
-            QtdPessoas = 250,
-            DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-            Tema = "Técnologia",
-            ImagemURL = "Foto.png"
-            },
-            new Evento () {
-            EventoId = 2,
-            Local = "Jardim Alvorada",
-            Lote = "2º Lote",
-            QtdPessoas = 150,
-            DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-            Tema = "Técnologia",
-            ImagemURL = "Foto.png"
-            }
-            };
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
-         [HttpPost]
+        [HttpPost]
         public string Post()
         {
             return "Valor de Post";
         }
-         [HttpPut("{id}")]
+        [HttpPut("{id}")]
         public string Put(int id)
         {
             return $"Valor de Put com id = {id}";
         }
-         [HttpDelete("{id}")]
+        [HttpDelete("{id}")]
         public string Delete(int id)
         {
             return $"Valor de Delete com id = {id}";
         }
     }
-    
+
 }
---
+++ Back/src/EvenTec.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using EvenTec.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvenTec.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
